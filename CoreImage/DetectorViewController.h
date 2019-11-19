2bab5cacf194a69f1dc939e50afae6af42523acd
张奥 <2515912396@qq.com>
2019-11-19 17:04:03 +0800

    提交

---
+++ CoreImage/DetectorViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetectorViewController.h
+//  CoreImage
+//
+//  Created by 廖文韬 on 2018/1/31.
+//  Copyright © 2018年 lisa. All rights reserved.
+//
+//  特征识别
+
+#import <UIKit/UIKit.h>
+
+@interface DetectorViewController : UIViewController
+
+@end
---
+++ CoreImage/ViewController.m
@@ -0,0 +1,64 @@
+//
+//  ViewController.m
+//  CoreImage
+//
+//  Created by lina on 2018/1/25.
+//  Copyright © 2018年 lisa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    //失真效果 改变几何形状创建3D效果
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryDistortionEffect;
+//    //扭曲图片和纠正源图像问题，例如仿射变换来校正相对于地平线旋转的图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryGeometryAdjustment;
+//    //合成滤镜，操作两个图像源
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryCompositeOperation;
+//    //半色调效果
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryHalftoneEffect;
+//    //色彩调整，用于消除色彩偏移、校正亮度和对比度
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryColorAdjustment;
+//    //修改图像颜色，我们一般用的比较多，类似美图工具的滤镜效果
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryColorEffect;
+//    //
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryTransition;
+//    //瓦片效果 平铺图片
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryTileEffect;
+//    //产生图案的过滤器，如纯色、棋盘或星星的光泽。生成的输出通常用作对另一个过滤器的输入。
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryGenerator;
+//    //减少图像数据 解决图像分析问题
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryReduction NS_AVAILABLE(10_5, 5_0);
+//    //渐变效果
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryGradient;
+//    //绘画风格
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryStylize;
+//    //锐化图像 锐化掩模和提高亮度。
+//    CORE_IMAGE_EXPORT NSString * const kCICategorySharpen;
+//    //柔滑图像，主要用于模糊图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryBlur;
+//    //处理视频图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryVideo;
+//    //处理静态图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryStillImage;
+//    //处理交错图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryInterlaced;
+//    //处理非方形图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryNonSquarePixels;
+//    //处理高动态图像
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryHighDynamicRange;
+//    //用于区分built-in filters  plug-in filters.
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryBuiltIn;
+//    //链接几个过滤器
+//    CORE_IMAGE_EXPORT NSString * const kCICategoryFilterGenerator NS_AVAILABLE(10_5, 9_0);
+    
+}
+@end
